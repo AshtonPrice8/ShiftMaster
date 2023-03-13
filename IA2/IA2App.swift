d8901bb27a0fbda8c6b9a7d240ebac7232e2f369
Oliver Baumeister <oliver.baumeister@gmail.com>
2023-03-13 14:03:51 +1000

    Initial Commit

---
+++ IA2/IA2App.swift
@@ -0,0 +1,17 @@
+//
+//  IA2App.swift
+//  IA2
+//
+//  Created by Oliver Baumeister on 13/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct IA2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
